97aabeb5c4afa1a09223f39a335f7a40f1b16511
Mr Jumba <flaviajumba@gmail.com>
2024-10-06 22:46:01 +0300

    added the relevant windspeed and temp images

--- Weather_app/src/components/Weather.jsx
+++ Weather_app/src/components/Weather.jsx
@@ -7,7 +7,7 @@ import drizzle_icon from '../assets/drizzle.png';
 import rain_icon from '../assets/rain.png';
 import snow_icon from '../assets/snow.png';
 import wind_icon from '../assets/wind.png';
-
+import humidity_icon from '../assets/humidity.png';
 const Weather = () => {  // Changed 'function Weather =' to 'const Weather ='
   return (
     <div className='weather'> 
@@ -15,7 +15,26 @@ const Weather = () => {  // Changed 'function Weather =' to 'const Weather ='
         <input type="text" placeholder='search'/>
         <img src={search_icon} alt="Search Icon" />  {/* Changed to use curly braces for the image source */}
       </div>
-      <img src="" alt="" />
+      <img src= {clear_icon} className='weather-icon' alt="" />
+      <p className='temperature'>16c</p>
+      <p className='location'>London</p>
+      <div className='weather-data'>
+        <div className='col'>
+           <img src= {humidity_icon} alt="" />
+           <div>
+            <p>91%</p>
+            <span>Humidity</span>
+           </div>
+        </div>
+
+        <div className='col'>
+           <img src= {wind_icon} alt="" />
+           <div>
+            <p>3.4km/h</p>
+            <span>wind speed</span>
+           </div>
+        </div>
+      </div>
     </div>
   );
 }
