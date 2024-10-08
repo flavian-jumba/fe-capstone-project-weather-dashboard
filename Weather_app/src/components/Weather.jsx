e3d291934e3f0c46f9a16369a106a49dcfc67f9e
Flavian Jumba <flaviajumba@gmail.com>
2024-10-08 09:47:47 +0300

    npm run build

--- Weather_app/src/components/Weather.jsx
+++ Weather_app/src/components/Weather.jsx
@@ -25,6 +25,7 @@ const Weather = () => {
     // Function to fetch weather data based on city name
     const search = async (city) => {
         try {
+            
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
             const response = await fetch(url);
             const data = await response.json();
